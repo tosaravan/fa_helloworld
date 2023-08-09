243a8cad9c6e1bc856f70d5273a5e7d9fef17d60
Kamleshwar Parivallal <kamleshwar@trainingpeople.in>
2023-08-09 11:24:22 +0100

    Starting with Bigger Application - Multiple Files  - Fast API

--- main.py
+++ main.py
@@ -361,3 +361,7 @@ async def hello(name: str = Path(..., min_length=3, max_length=10)):
 @app.get("/hello/{name}/{age}")
 async def hello(name: str = Path(..., min_length=3, max_length=10), age: int = Path(..., ge=1, le=100)):
     return {"name": name, "age": age}
+
+
+
+# Starting with Bigger Application - Multiple Files
