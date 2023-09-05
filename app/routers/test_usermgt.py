b2a2452211d83b3f1a20408dfcd41060da27db28
Kamleshwar Parivallal <kamleshwar@trainingpeople.in>
2023-09-05 03:28:13 +0100

    Test for Create Shopping Cart - Failed

--- app/routers/test_usermgt.py
+++ app/routers/test_usermgt.py
@@ -9,6 +9,21 @@ client = TestClient(app)
 
 user_payload = {"firstname": "foobar", "lastname": "Foo Bar", "email": "The Foo Barters", "mobile": "Foo Bar",
                 "password": "Foo Bar"}
+cart_payload = {
+        "customer_name": "Kamleshwar",
+        "items": [
+            {
+                "product_name": "One Plus 9 Pro",
+                "product_cost": 75000,
+                "total_units": 1
+            },
+            {
+                "product_name": "Phone Case ",
+                "product_cost": 1500,
+                "total_units": 2
+            }
+        ]
+    }
 
 
 @pytest.fixture()
@@ -31,3 +46,27 @@ def test_create_user(test_db):
                                "is_active": True,
                                "lastname": "Foo Bar",
                                "mobile": "Foo Bar"}
+
+def test_create_shopping_cart(test_db):
+    response = client.post(
+        "/usermgt/cart",
+        json=cart_payload,
+    )
+    assert response.status_code == 200
+    assert response.json()=={"customer_name": "Kamleshwar",
+        "items": [
+            {
+                "product_name": "One Plus 9 Pro",
+                "product_cost": 75000,
+                "total_units": 1
+            },
+            {
+                "product_name": "Phone Case ",
+                "product_cost": 1500,
+                "total_units": 2
+            }
+        ]
+
+    }
+
+
